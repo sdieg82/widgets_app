9b83c46d3796c6174db7227d0ab18e1b4e8b18f4
Diego <sdieg82@gmail.com>
2025-08-11 17:03:05 -0500

    menu items

---
+++ lib/config/themes/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const colorList=<Color>[
+   Colors.blue,
+   Colors.yellow,
+   Colors.red,
+   Colors.purple,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({
+    this.selectedColor=0
+  }):assert(
+    selectedColor>=0 && selectedColor <= colorList.length
+  );
+ThemeData getTheme()=>ThemeData(
+   useMaterial3: true,
+   colorSchemeSeed: colorList[selectedColor],
+   appBarTheme: const AppBarTheme(
+    centerTitle: false,
+   )
+);
+}
+
