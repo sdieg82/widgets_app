95cd14be4a9ae144654f658e8d1a37c28da9d801
Diego <sdieg82@gmail.com>
2025-08-13 10:09:01 -0500

    New widgets

--- lib/presentation/screens/screens_export.dart
+++ lib/presentation/screens/screens_export.dart
@@ -1,3 +1,9 @@
+export 'package:widgets_app/presentation/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/infinite_scroll/infinite_scroll.dart';
+export 'package:widgets_app/presentation/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/ui/ui_controls_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home_screen.dart';
