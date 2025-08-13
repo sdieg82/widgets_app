91c713f69308766215f9699831457dc7b5ed5598
Diego <sdieg82@gmail.com>
2025-08-13 09:40:43 -0500

    barril creado, screens creados

---
+++ lib/presentation/ui/ui_controls_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UicontrolsScreen extends StatelessWidget {
+  static const name='uicontrols_screen';
+  const UicontrolsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
