8cccf209b879b325cd2fbff019b0a2eaacdf9ed7
Diego <sdieg82@gmail.com>
2025-08-14 08:40:13 -0500

    Animated screen

--- lib/presentation/animated/animated_screen.dart
+++ lib/presentation/animated/animated_screen.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
 
-class AnimatedScreen extends StatelessWidget {
+
+class AnimatedScreen extends StatefulWidget {
   static const name='animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  
+  double width=50;
+  double height=50;
+  Color color=Colors.indigo;
+  double borderRadius=10.0;
+
+  void changeShape(){
+    final random=Random();
+
+    setState(() {
+      width=random.nextInt(300)+50;
+      height=random.nextInt(200)+60;
+      borderRadius=random.nextInt(105)+5;
+      color=Color.fromRGBO(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextDouble());
+    },
+    
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,18 +37,18 @@ class AnimatedScreen extends StatelessWidget {
         title: Text('Desde animated'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: changeShape,
         child: Icon(Icons.play_arrow_rounded),
       ),
       body: Center(
         child: AnimatedContainer(
           duration: Duration(seconds: 1),
           curve: Curves.easeOutCubic,
-          width: 100,
-          height: 100,
+          width: width <= 0 ? 10 : width,
+          height: height <= 0 ? 10 : height,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius < 0 ? 10 : borderRadius)
           ),
         ),
       ),
