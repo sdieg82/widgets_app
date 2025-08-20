ae9f57652828b7fc251a36d281408df44f52ad26
Diego <sdieg82@gmail.com>
2025-08-20 13:06:08 -0500

    set state appbar, nav bar

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_item.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -12,6 +15,12 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    final hasNoctch=MediaQuery.of(context).viewPadding.top>35;
+    if (Platform.isAndroid) {
+      print('Android $hasNoctch');    
+    }else{
+      print('Is IOS $hasNoctch' );
+    }
     return NavigationDrawer(
       onDestinationSelected: (value) {
         setState(() {
@@ -20,14 +29,42 @@ class _SideMenuState extends State<SideMenu> {
       },
       selectedIndex: navDrawerIndex,
       children:[
-         NavigationDrawerDestination(
-          icon: Icon(Icons.add_box_outlined), 
-          label: Text('Home Screen NavBar'),
+        Padding(
+          padding:EdgeInsets.fromLTRB(28, hasNoctch? 0: 20, 16, 10),
+          child: Text('Menú', ),
         ),
-         NavigationDrawerDestination(
-          icon: Icon(Icons.shop_outlined), 
-          label: Text('Shop')
+        Divider(),
+        ...appMenuItems
+        .sublist(0,3)
+        .map((e)=> 
+        NavigationDrawerDestination(
+          icon: Icon(e.icon), 
+          label: Text(e.title),
+        )),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
         ),
+
+        Padding(
+           padding:EdgeInsets.fromLTRB(28, hasNoctch? 0: 20, 16, 10),
+          child: Text('Otras opciones', ),
+        ),
+
+         Divider(),
+        ...appMenuItems
+        .sublist(3)
+        .map((e)=> 
+        NavigationDrawerDestination(
+          icon: Icon(e.icon), 
+          label: Text(e.title),
+        )),
+        Padding(
+          padding: EdgeInsetsGeometry.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
+        )
+
+
       ] 
     );
   }
